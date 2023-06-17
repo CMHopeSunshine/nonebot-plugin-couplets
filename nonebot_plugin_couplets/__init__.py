e4ba6b9ef0edee80e90342f76352f47542096b36
CMHopeSunshine <277073121@qq.com>
2023-06-17 15:43:53 +0800

    ✨ 适配所有适配器，适配新版元数据

--- nonebot_plugin_couplets/__init__.py
+++ nonebot_plugin_couplets/__init__.py
@@ -1,9 +1,6 @@
-from typing import List
-
 import httpx
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Message
-from nonebot.adapters.onebot.v11.helpers import Numbers
+from nonebot.adapters import Message
 from nonebot.params import CommandArg
 from nonebot.plugin import PluginMetadata
 from nonebot.typing import T_State
@@ -11,7 +8,10 @@ from nonebot.typing import T_State
 __plugin_meta__ = PluginMetadata(
     name="对对联",
     description="人工智能和你对对联",
-    usage="对联<内容>(数量)",
+    usage="对联<内容> (数量)",
+    type="application",
+    homepage="https://github.com/CMHopeSunshine/nonebot-plugin-couplets",
+    supported_adapters=None,
     extra={
         "author":  "惜月 <277073121@qq.com>",
         "version": "1.0.0",
@@ -19,22 +19,24 @@ __plugin_meta__ = PluginMetadata(
 )
 
 couplets = on_command('对联', aliases={'对对联'}, priority=13, block=True, state={
-    'pm_usage': '对联<内容>(数量)',
+    'pm_usage': '对联<内容> (数量)',
     'pm_describe': '人工智能和你对对联',
     'pm_priority': 15
 })
 
 
 @couplets.handle()
-async def _(state: T_State, msg: Message = CommandArg(), num: List[float] = Numbers()):
-    msg = msg.extract_plain_text().strip()
-    if msg:
-        for n in num:
-            msg = msg.replace(str(int(n)), '')
-    if msg:
-        state['text'] = msg
+async def _(state: T_State, msg: Message = CommandArg()):
+    msg_text = msg.extract_plain_text().strip()
+    args = msg_text.split(' ', 1)
+    if len(args) == 2 and args[1].isdigit():
+        num = int(args[1])
+    else:
+        num = None
+    if args[0]:
+        state['text'] = args[0]
     if num:
-        state['num'] = int(num[0]) if num[0] <= 10 else 10
+        state['num'] = min(num, 10)
     else:
         state['num'] = 1
 
